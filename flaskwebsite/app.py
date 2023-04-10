f8a08c9af2c8b67fdbb32eefb0c1cfda63b35ec2
Bryan Nguy <bryan123four@gmail.com>
2023-04-09 20:36:23 -0400

    successfully display protein table on website

--- flaskwebsite/app.py
+++ flaskwebsite/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_navigation import Navigation
+import psycopg2
 
 app = Flask(__name__)
 
@@ -7,17 +7,36 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/about")
+@app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
 
-@app.route("/base")
+@app.route('/base')
 def base():
-    return render_template("base.html")
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        host="localhost",
+        database="flask_db",
+        # user=os.environ['DB_USERNAME'],
+        # password=os.environ['DB_PASSWORD']
+        user = "postgres",
+        password = "123")
 
-@app.route("/guide")
+    # Retrieve the data from the database
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM \"PROTEIN_INFO\"")
+    data = cur.fetchall()
+
+    # Create the context dictionary
+    # context = {'my_data': data}
+    context = {'base': data}
+
+    # Render the template and pass in the context dictionary
+    return render_template('base.html', **context)
+
+@app.route('/guide')
 def guide():
-    return render_template("guide.html")
+    return render_template('guide.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
