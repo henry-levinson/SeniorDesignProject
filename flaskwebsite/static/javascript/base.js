494356c27249bd1aa016495d2164f83bdff0a860
henry-levinson <henrylevinson6@gmail.com>
2023-04-12 14:52:33 -0400

    Added previous and next buttons

--- flaskwebsite/static/javascript/base.js
+++ flaskwebsite/static/javascript/base.js
@@ -1,37 +1,74 @@
 $(document).ready(function () {
-    // Initialize star icons
-    $(document).on('click', '.star', function() {
-      $(this).addClass('active');
-      $(this).prevAll('.star').addClass('active');
-      $(this).nextAll('.star').removeClass('active');
-      $(this).siblings('.star').attr('data-rating', '0');
-      $(this).attr('data-rating', '1');
-      
-      // Get the row index and ranking value
-      var rowIndex = $(this).closest('tr').index();
-      var ranking = $(this).index() + 1;
-      
-      // Save the ranking in local storage
-      localStorage.setItem('ranking_' + rowIndex, ranking);
-    });
-    
-    // Load saved rankings from local storage
-    $('.star').each(function() {
-      var rowIndex = $(this).closest('tr').index();
-      var savedRanking = localStorage.getItem('ranking_' + rowIndex);
-      if (savedRanking) {
-        $(this).siblings('.star').removeClass('active');
-        $(this).removeClass('active');
-        $(this).siblings('.star').attr('data-rating', '0');
-        $(this).attr('data-rating', savedRanking);
-        $(this).prevAll('.star').addClass('active');
+    var rowsPerPage = 25; // Number of rows to display per page
+    var currentPage = 1; // Current page number
+    var totalRows = $('.dataframe tbody tr').length; // Total number of rows
+  
+    // Calculate total number of pages
+    var totalPages = Math.ceil(totalRows / rowsPerPage);
+  
+    // Function to show rows for current page
+    function showRows() {
+      $('.dataframe tbody tr').hide(); // Hide all rows
+      var startIndex = (currentPage - 1) * rowsPerPage; // Calculate starting index
+      var endIndex = startIndex + rowsPerPage; // Calculate ending index
+      $('.dataframe tbody tr').slice(startIndex, endIndex).show(); // Show rows for current page
+    }
+  
+    // Show initial rows for first page
+    showRows();
+  
+    // Function to update pagination buttons
+    function updatePaginationButtons() {
+      // Enable/disable previous button
+      if (currentPage === 1) {
+        $('.pagination button.previous').prop('disabled', true);
+      } else {
+        $('.pagination button.previous').prop('disabled', false);
+      }
+  
+      // Enable/disable next button
+      if (currentPage === totalPages) {
+        $('.pagination button.next').prop('disabled', true);
+      } else {
+        $('.pagination button.next').prop('disabled', false);
+      }
+    }
+  
+    // Function to update drop-down menu
+    function updateDropdownMenu() {
+      // Set drop-down menu value to current rowsPerPage
+      $('.dropdown select').val(rowsPerPage);
+    }
+  
+    // Update pagination buttons and drop-down menu on page load
+    updatePaginationButtons();
+    updateDropdownMenu();
+  
+    // Event handler for next button
+    $('.pagination button.next').on('click', function () {
+      if (currentPage < totalPages) {
+        currentPage++; // Increment current page
+        showRows(); // Show rows for current page
+        updatePaginationButtons(); // Update pagination buttons
       }
     });
-    
-    // Reset star icons
-    $('.stars').mouseleave(function() {
-      $(this).find('.star').removeClass('active');
-      $(this).find('.star').attr('data-rating', '0');
+  
+    // Event handler for previous button
+    $('.pagination button.previous').on('click', function () {
+      if (currentPage > 1) {
+        currentPage--; // Decrement current page
+        showRows(); // Show rows for current page
+        updatePaginationButtons(); // Update pagination buttons
+      }
+    });
+  
+    // Event handler for drop-down menu
+    $('.dropdown select').on('change', function () {
+      rowsPerPage = parseInt($(this).val()); // Update rowsPerPage with selected value
+      totalPages = Math.ceil(totalRows / rowsPerPage); // Recalculate total number of pages
+      currentPage = 1; // Reset current page to 1
+      showRows(); // Show rows for current page
+      updatePaginationButtons(); // Update pagination buttons
     });
   });
   
